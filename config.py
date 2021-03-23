7f6236a4c7e63e5d68a5d5792f3a411fa52bcf64
AleksanderZhariuk <65425531+AleksanderZhariuk@users.noreply.github.com>
2021-03-23 20:29:41 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+MY_LAT = None # Your latittude | FLOAT
+MY_LONG = None # Your longtitude | FLOAT
+MY_EMAIL = None # Your working email | STRING
+MY_PASSWORD = None # Your email password | STRING
+
+# https://www.latlong.net
